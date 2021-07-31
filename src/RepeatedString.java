e248f4dcec440b4166916f7ee4f262403f43a09b
Shivam Barsainya <shivambarsainya@yahoo.com>
2021-07-31 18:34:22 +0530

    RepeatedString

---
+++ src/RepeatedString.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(String s, long n) {
+        long strLength = s.length();
+        long totalLoops =  (n / strLength);
+        int remaining = (int) (n % strLength);
+        long localStr = s.chars().filter(value -> value == 'a').count();
+        long count = localStr * totalLoops;
+        count = count + s.substring(0, remaining).chars().filter(value -> value == 'a').count();
+        return count;
+    }
+
+}
+
+public class RepeatedString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
